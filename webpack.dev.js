3e8a3fb0c8d57f483571e317423f2185e66395ae
Steven Thompson <capitalste@gmail.com>
2018-03-05 08:07:28 +0000

    Started a move towards Handlebars for docs.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
 
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
@@ -16,6 +17,25 @@ module.exports = merge(common, {
     port: 9000
   },
   plugins: [
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin('bundle.css'),
+    new HandlebarsPlugin({
+      entry: path.join(process.cwd(), "src", "views", "*.hbs"),
+      output: path.join(process.cwd(), "docs", "[name].html"),
+      //data: path.join(__dirname, "src", "views", "data.json"),
+      data: require("./src/views/data.json"),
+      partials: [
+          path.join(process.cwd(), "src", "views", "components", "*", "*.hbs")
+      ],
+      helpers: {
+          nameOfHbsHelper: Function.prototype,
+          projectHelpers: path.join(process.cwd(), "app", "helpers", "*.helper.js")
+      },
+      onBeforeSetup: function (Handlebars) {},
+      onBeforeAddPartials: function (Handlebars, partialsMap) {},
+      onBeforeCompile: function (Handlebars, templateContent) {},
+      onBeforeRender: function (Handlebars, data) {},
+      onBeforeSave: function (Handlebars, resultHtml, filename) {},
+      onDone: function (Handlebars, filename) {}
+  })
   ]
 });
