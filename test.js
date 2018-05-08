2407691d36eee2061bff744011ad2b0c7ce89a9d
Steven Thompson <capitalste@gmail.com>
2018-05-08 22:04:28 +0100

    Added a test for ensuring the corresponding <select> value is set when a dropdown item is clicked. The test runs under various plugin scenarios.

--- test.js
+++ test.js
@@ -1,14 +1,27 @@
-import { Selector } from 'testcafe';
+import {
+	Selector
+} from 'testcafe';
 
-fixture `Test Bootstrap Select Dropdown instance`
-    .page `http://localhost:9000`;
+fixture `Test Bootstrap Select Dropdown instances with varying plugin parameter combinations for regular selects, multiselects and selects/multiselects with optgroups.`
+	.page `http://localhost:8080/tests.html`;
 
-const demoOverview = Selector('#demo_overview');
-const demoOverviewDropdownItem = demoOverview.find('#bsd1-container .dropdown-item').withText('Cambodia');
+let instanceCount = 12;
 
-test(`Select an option from the drop-down menu`, async t => {
-    await t
-        .click(citySelect)
-        .click(cityOption.withText('London'))
-        .expect(citySelect.value).eql('London');
+test(`Select (click) a random option from each drop-down menu and ensure the corresponding value is set on the <select> element.`, async t => {
+	let i;
+	for (i = 0; i < instanceCount; i++) {
+		let selectElement = Selector('#test_' + i);
+		let dropdownButton = Selector('#bsd' + (i + 1) + '-button');
+		let dropdownItems = Selector('#bsd' + (i + 1) + '-container').find('.dropdown-item');
+		let options = selectElement.find('option');
+		let length = await options.count;
+		let n = Math.floor(Math.random() * length);
+		let text = await options.nth(n).innerText;
+		let value = await options.nth(n).value;
+		console.log(' Test ' + i + ': Select "' + text + '" (' + value + ').');
+		await t
+			.click(dropdownButton)
+			.click(dropdownItems.withText(text))
+			.expect(selectElement.value).eql(value);
+	}
 });
